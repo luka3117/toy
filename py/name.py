982d170c3d3519a209be7dbc10f1626a38f7d024
Jongchan Lee <jclee@JongchannoMacBook-Air.local>
2020-03-29 22:11:54 +0900

    7

---
+++ py/name.py
@@ -0,0 +1,15 @@
+def aa(x):
+    return x+1
+
+if __name__=="__main__":
+    print(aa(1))
+
+import os
+
+
+print(os.listdir())
+
+
+
+print(os.path.exists('sample2.py'))
+os.rename('./sample2.py','./ddccc.py')
--- py/sample1.py
+++ py/sample1.py
@@ -1,47 +1,22 @@
-#-*- coding: utf-8 -*-
-import os
-import re
-import sys
+# coding: utf-8
 
-import math
+import numpy as np
+import matplotlib.pyplot as plt
 
-
-print(type(math))
-print(math)
-
-print(math.radians)
-
-print(type(math.radians))
+plt.__file__
+matplotlib.pyplot.__file__
 
 
-import sklearn
-
-aa=print(sklearn)
-
-aa=sklearn.__file__
-
-print(aa)
-
-math.__file__
-
-import pprint
-aa=pprint.pprint(math)
+np.__file__
 
 
-print(math)
-print(sys)
-
-print(type(sklearn))
-
-sklearn.__file__
-
-print(re)
-re.__file__
+print(np.random.rand)
 
 
+# 乱数を生成
+x = np.random.rand(10)
+y = np.random.rand(10)
 
-
-
-import aaa
-
-print(aaa)
+# 散布図を描画
+# plt.show()
+plt.scatter(x, y)
